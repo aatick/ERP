81ec3f6ede238f691f8c44a25fdc5c771e0545d6
aatick <aatick.ru3@gmail.com>
2017-12-29 18:37:20 +0600

    Add files via upload

---
+++ ERP.Web/Areas/Upms/UpmsAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using ERP.Web;
+
+namespace ERP.Web
+{
+    public class UpmsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Upms";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Upms_default",
+                "Upms/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }, new string[] { "Upms.Controllers" }
+            );
+        }
+    }
+}
