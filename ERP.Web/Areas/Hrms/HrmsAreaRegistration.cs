ec119f6c30075dcde455ccfe21860d1549ba48e7
aatick <aatick.ru3@gmail.com>
2017-12-29 18:27:27 +0600

    Add files via upload

---
+++ ERP.Web/Areas/Hrms/HrmsAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace Hrms
+{
+    public class HrmsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Hrms";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Hrms_default",
+                "Hrms/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "Hrms.Controllers" }
+            );
+        }
+
+    }
+}
