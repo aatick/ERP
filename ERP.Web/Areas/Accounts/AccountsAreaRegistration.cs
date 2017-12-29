2fd828b21efa6180ad1391ee5f8f5f5a393c4a4a
aatick <aatick.ru3@gmail.com>
2017-12-29 18:23:47 +0600

    Add files via upload

---
+++ ERP.Web/Areas/Accounts/AccountsAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using ERP.Web;
+
+namespace ERP.Web
+{
+    public class AccountsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Accounts";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Accounts_default",
+                "Accounts/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }, new string[] { "Accounts.Controllers" }
+            );
+        }
+    }
+}
